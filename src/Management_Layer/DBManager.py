2171c1dbed07b9714f05ce868e3ddc6c22a44d23
Ex-voda <lsh301705@gmail.com>
2024-07-30 16:56:36 +0800

    管理层更新：完成了控制端的主要方法入口

--- src/Management_Layer/DBManager.py
+++ src/Management_Layer/DBManager.py
@@ -23,8 +23,11 @@ class DBManager():
     def __init__(self, db_params):
         self.db_params = db_params
     @connect_db_method
-    def get_project_config(self, cursor, project_name):
-        print(project_name)
+    def get_project_config_path(self, cursor, project_name):
         cursor.execute("SELECT config_path FROM config WHERE project = %s", (project_name,))
         # print(cursor.fetchone())
         return cursor.fetchone()[0]
+    
+    @connect_db_method
+    def update_project_config_path(self, cursor, project_name, config_path):
+        cursor.execute("UPDATE config SET config_path = %s WHERE project = %s", (config_path, project_name))
