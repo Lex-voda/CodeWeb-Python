9b5066ac0f66242043313be4f80ec2da8e5baab1
Timelordregeneration10 <3497049745@qq.com>
2024-07-30 20:03:30 +0800

    edit modal

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -34,8 +34,8 @@ export default async function Home() {
             <div className="w-full max-w-xl h-[600px] no-scrollbar overflow-scroll flex flex-col items-center gap-6">
                 {projectList.map((project: string) =>
                 (
-                    <div key={project} className="w-full min-h-32 bg-white rounded-lg shadow-md flex justify-center items-center">
-                        <p>{project}</p>
+                    <div key={project} className="w-full min-h-32 bg-white rounded-lg shadow-md flex justify-center items-center cursor-pointer">
+                        <a href={`/project/${project}`}>{project}</a>
                     </div>)
                 )}
             </div>
